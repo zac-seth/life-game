cb354247f71c312b40601317a8fcef4e3c1060ba
Zac Seth <zac.seth@gmail.com>
2017-05-08 12:15:36 +0200

    Created a modal component and the create habit view.

--- vue/src/app.vue
+++ vue/src/app.vue
@@ -1,24 +1,42 @@
 <template>
-  <div class="app" id="you-quest">
-    <h1 class="title">Welcome to YouQuest</h1>
-    <router-view class="primary-window"></router-view>
-  </div>
+    <div class="app" id="you-quest">
+        <h1 class="title">Welcome to YouQuest</h1>
+        <router-view class="primary-window"></router-view>
+      <create-habit></create-habit>
+    </div>
 </template>
 
 <script>
+import store from "@/stores"
+import createHabit from "@/views/quests/habits/create-habit"
+
 export default {
-  name: 'you-quest'
+    name: 'you-quest',
+
+    store,
+
+    components: {
+        "create-habit": createHabit
+    },
+
+    created() {
+        window.addEventListener("keyup", (evt) => {
+            if (evt.keyCode === 27) {
+                this.$store.dispatch("view/cancelView")
+            }
+        })
+    }
 }
 </script>
 
 <style>
 .app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 800px;
-  margin: 60px auto;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 800px;
+    margin: 60px auto;
 }
 </style>
